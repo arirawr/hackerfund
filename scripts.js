888500670faeabc5d32a1ebbe707d2e502935a11
Arielle VanIderstine <arielle1459@gmail.com>
2015-04-07 14:47:58 -0400

    periodic update

--- scripts.js
+++ scripts.js
@@ -1,20 +1,28 @@
 
 var tabs = document.getElementsByClassName('tab');
 var contents = document.getElementsByClassName('content');
+var blocks = document.getElementsByClassName('block');
 
-adjustWidths();
+//adjustWidths();
 
 /*for (var i = 0; i < tabs.length; i++) {
 	tabs[i].addEventListener("mouseover", expand(tabs[i]));
 };*/
 
-function adjustWidths() {
+/*function adjustWidths() {
 	for (var i = 0; i < tabs.length; i++) {
-		width = parseInt(window.innerWidth) - parseInt(getComputedStyle(tabs[i], null).width);
+		width = parseInt(window.innerWidth) - parseInt(getComputedStyle(tabs[i], null).width) - parseInt(getComputedStyle(blocks[i], null).width);
 		contents[i].style.width = width + "px";
 	};
-}
-
-/*function expand(tab) {
-	alert("hovering on " + tab);
 }*/
+
+$('section').hover(function() {
+	$(this).children('.tab').toggleClass("zero-width");
+	$(this).children('.block').toggleClass("auto-width");
+	$(this).children('.content').toggleClass("white-section");
+	//adjustWidths();
+});
+
+//$('section').hover(adjustWidths());
+
+
